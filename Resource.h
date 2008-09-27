9df4feba25cfb238446264f93bcbaff1f5551860
Admin <admin@chrisoldwood.com>
2008-09-27 21:37:49 +0000

    Added find dialog and menu commands.

--- Resource.h
+++ Resource.h
@@ -18,9 +18,15 @@
 #define ID_FILE_MRU_3                   108
 #define ID_FILE_MRU_4                   109
 #define ID_FILE_EXIT                    110
+#define IDD_NODE_PATH                   132
+#define IDD_FIND                        133
+#define ID_EDIT_POPUP                   200
+#define ID_EDIT_FIND                    201
+#define ID_EDIT_FIND_NEXT               202
 #define ID_VIEW_POPUP                   300
 #define ID_VIEW_HORZ                    301
 #define ID_VIEW_VERT                    302
+#define ID_VIEW_NODE_PATH               303
 #define ID_HELP_POPUP                   900
 #define ID_HELP_CONTENTS                901
 #define ID_HELP_ABOUT                   902
@@ -28,6 +34,8 @@
 #define IDC_EMAIL                       1084
 #define IDC_VERSION                     1085
 #define IDC_COPYRIGHT                   1086
+#define IDC_PATH                        1087
+#define IDC_EDIT1                       1088
 #define IDD_MAIN                        5000
 #define IDD_ABOUT                       5001
 #define IDC_STATIC                      -1
@@ -37,9 +45,9 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        132
-#define _APS_NEXT_COMMAND_VALUE         168
-#define _APS_NEXT_CONTROL_VALUE         1087
+#define _APS_NEXT_RESOURCE_VALUE        134
+#define _APS_NEXT_COMMAND_VALUE         173
+#define _APS_NEXT_CONTROL_VALUE         1089
 #define _APS_NEXT_SYMED_VALUE           104
 #endif
 #endif
